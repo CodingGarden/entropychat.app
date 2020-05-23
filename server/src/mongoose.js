8499c751202487c5f5f9543d83b235c22aeecbb2
w3cj <cj@null.computer>
2020-05-23 13:07:34 -0600

    :sparkles: Add docker-compose to server

--- server/src/mongoose.js
+++ server/src/mongoose.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const logger = require('./logger');
 
 module.exports = function (app) {
+  const mongodb = app.get('mongodb');
+  const password = encodeURIComponent(mongodb.root_password);
+  const connectionString = `mongodb://${mongodb.root_user}:${password}@${mongodb.host}:${mongodb.port}/${mongodb.db_name}?authSource=admin`;
   mongoose.connect(
-    app.get('mongodb'),
+    connectionString,
     { useCreateIndex: true, useNewUrlParser: true }
   ).catch(err => {
     logger.error(err);
