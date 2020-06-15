ece4c62c149872b34875a65275dd84dedea02cd2
CallumChaney <callumchaney8@gmail.com>
2020-06-15 18:16:53 +0100

    modified:   server/ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: 'api',
       script: './src/index.js',
-      instance_var: '1',
       watch: true,
       ignore_watch: ['node_modules', 'docker-data'],
     },
